adcecd476a1a5d51cd86e27b5d14d4e2f5ba1219
Rohan7287 <148252250+Rohan7287@users.noreply.github.com>
2024-01-20 15:42:50 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -448,7 +448,9 @@ def humanbytes(size):
         size /= power
         n += 1
     return str(round(size, 2)) + " " + Dic_powerN[n] + 'B' 
+#######################################################################
 
+######################################################
 
 async def get_shortlink(link):
     https = link.split(":")[0]
